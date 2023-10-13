45ef9d7d2cff5279b63fc11b2461f3ba4d7d38e1
Sebastian Ortega <sebastian.ortega.fernandezz@gmail.com>
2023-10-13 19:45:08 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
+const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text > You Clicked { count } times</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setCount(count + 1)}
+      >
+        <Text>
+          Click Me +
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setCount(count - 1)}
+      >
+        <Text>
+          Click Me -
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setCount(0)}
+      >
+        <Text>
+          Click Me For Reset
+        </Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
+// React Native Styles: 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#C2F169',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 50,
+
+  }
+
+})
+
+export default App;
